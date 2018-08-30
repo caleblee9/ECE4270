305a2743ea6319dc75fa7575937c775d50e0bdd3
Caleb <caleblee3@gmail.com>
2018-08-30 17:44:16 -0500

    Lab1

---
+++ mu-mips.c
@@ -0,0 +1,455 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include "mu-mips.h"
+
+/***************************************************************/
+/* Print out a list of commands available                                                                  */
+/***************************************************************/
+void help() {        
+	printf("------------------------------------------------------------------\n\n");
+	printf("\t**********MU-MIPS Help MENU**********\n\n");
+	printf("sim\t-- simulate program to completion \n");
+	printf("run <n>\t-- simulate program for <n> instructions\n");
+	printf("rdump\t-- dump register values\n");
+	printf("reset\t-- clears all registers/memory and re-loads the program\n");
+	printf("input <reg> <val>\t-- set GPR <reg> to <val>\n");
+	printf("mdump <start> <stop>\t-- dump memory from <start> to <stop> address\n");
+	printf("high <val>\t-- set the HI register to <val>\n");
+	printf("low <val>\t-- set the LO register to <val>\n");
+	printf("print\t-- print the program loaded into memory\n");
+	printf("?\t-- display help menu\n");
+	printf("quit\t-- exit the simulator\n\n");
+	printf("------------------------------------------------------------------\n\n");
+}
+
+/***************************************************************/
+/* Read a 32-bit word from memory                                                                            */
+/***************************************************************/
+uint32_t mem_read_32(uint32_t address)
+{
+	int i;
+	for (i = 0; i < NUM_MEM_REGION; i++) {
+		if ( (address >= MEM_REGIONS[i].begin) &&  ( address <= MEM_REGIONS[i].end) ) {
+			uint32_t offset = address - MEM_REGIONS[i].begin;
+			return (MEM_REGIONS[i].mem[offset+3] << 24) |
+					(MEM_REGIONS[i].mem[offset+2] << 16) |
+					(MEM_REGIONS[i].mem[offset+1] <<  8) |
+					(MEM_REGIONS[i].mem[offset+0] <<  0);
+		}
+	}
+	return 0;
+}
+
+/***************************************************************/
+/* Write a 32-bit word to memory                                                                                */
+/***************************************************************/
+void mem_write_32(uint32_t address, uint32_t value)
+{
+	int i;
+	uint32_t offset;
+	for (i = 0; i < NUM_MEM_REGION; i++) {
+		if ( (address >= MEM_REGIONS[i].begin) && (address <= MEM_REGIONS[i].end) ) {
+			offset = address - MEM_REGIONS[i].begin;
+
+			MEM_REGIONS[i].mem[offset+3] = (value >> 24) & 0xFF;
+			MEM_REGIONS[i].mem[offset+2] = (value >> 16) & 0xFF;
+			MEM_REGIONS[i].mem[offset+1] = (value >>  8) & 0xFF;
+			MEM_REGIONS[i].mem[offset+0] = (value >>  0) & 0xFF;
+		}
+	}
+}
+
+/***************************************************************/
+/* Execute one cycle                                                                                                              */
+/***************************************************************/
+void cycle() {                                                
+	handle_instruction();
+	CURRENT_STATE = NEXT_STATE;
+	INSTRUCTION_COUNT++;
+}
+
+/***************************************************************/
+/* Simulate MIPS for n cycles                                                                                       */
+/***************************************************************/
+void run(int num_cycles) {                                      
+	
+	if (RUN_FLAG == FALSE) {
+		printf("Simulation Stopped\n\n");
+		return;
+	}
+
+	printf("Running simulator for %d cycles...\n\n", num_cycles);
+	int i;
+	for (i = 0; i < num_cycles; i++) {
+		if (RUN_FLAG == FALSE) {
+			printf("Simulation Stopped.\n\n");
+			break;
+		}
+		cycle();
+	}
+}
+
+/***************************************************************/
+/* simulate to completion                                                                                               */
+/***************************************************************/
+void runAll() {                                                     
+	if (RUN_FLAG == FALSE) {
+		printf("Simulation Stopped.\n\n");
+		return;
+	}
+
+	printf("Simulation Started...\n\n");
+	while (RUN_FLAG){
+		cycle();
+	}
+	printf("Simulation Finished.\n\n");
+}
+
+/***************************************************************/ 
+/* Dump a word-aligned region of memory to the terminal                              */
+/***************************************************************/
+void mdump(uint32_t start, uint32_t stop) {          
+	uint32_t address;
+
+	printf("-------------------------------------------------------------\n");
+	printf("Memory content [0x%08x..0x%08x] :\n", start, stop);
+	printf("-------------------------------------------------------------\n");
+	printf("\t[Address in Hex (Dec) ]\t[Value]\n");
+	for (address = start; address <= stop; address += 4){
+		printf("\t0x%08x (%d) :\t0x%08x\n", address, address, mem_read_32(address));
+	}
+	printf("\n");
+}
+
+/***************************************************************/
+/* Dump current values of registers to the teminal                                              */   
+/***************************************************************/
+void rdump() {                               
+	int i; 
+	printf("-------------------------------------\n");
+	printf("Dumping Register Content\n");
+	printf("-------------------------------------\n");
+	printf("# Instructions Executed\t: %u\n", INSTRUCTION_COUNT);
+	printf("PC\t: 0x%08x\n", CURRENT_STATE.PC);
+	printf("-------------------------------------\n");
+	printf("[Register]\t[Value]\n");
+	printf("-------------------------------------\n");
+	for (i = 0; i < MIPS_REGS; i++){
+		printf("[R%d]\t: 0x%08x\n", i, CURRENT_STATE.REGS[i]);
+	}
+	printf("-------------------------------------\n");
+	printf("[HI]\t: 0x%08x\n", CURRENT_STATE.HI);
+	printf("[LO]\t: 0x%08x\n", CURRENT_STATE.LO);
+	printf("-------------------------------------\n");
+}
+
+/***************************************************************/
+/* Read a command from standard input.                                                               */  
+/***************************************************************/
+void handle_command() {                         
+	char buffer[20];
+	uint32_t start, stop, cycles;
+	uint32_t register_no;
+	int register_value;
+	int hi_reg_value, lo_reg_value;
+
+	printf("MU-MIPS SIM:> ");
+
+	if (scanf("%s", buffer) == EOF){
+		exit(0);
+	}
+
+	switch(buffer[0]) {
+		case 'S':
+		case 's':
+			runAll(); 
+			break;
+		case 'M':
+		case 'm':
+			if (scanf("%x %x", &start, &stop) != 2){
+				break;
+			}
+			mdump(start, stop);
+			break;
+		case '?':
+			help();
+			break;
+		case 'Q':
+		case 'q':
+			printf("**************************\n");
+			printf("Exiting MU-MIPS! Good Bye...\n");
+			printf("**************************\n");
+			exit(0);
+		case 'R':
+		case 'r':
+			if (buffer[1] == 'd' || buffer[1] == 'D'){
+				rdump();
+			}else if(buffer[1] == 'e' || buffer[1] == 'E'){
+				reset();
+			}
+			else {
+				if (scanf("%d", &cycles) != 1) {
+					break;
+				}
+				run(cycles);
+			}
+			break;
+		case 'I':
+		case 'i':
+			if (scanf("%u %i", &register_no, &register_value) != 2){
+				break;
+			}
+			CURRENT_STATE.REGS[register_no] = register_value;
+			NEXT_STATE.REGS[register_no] = register_value;
+			break;
+		case 'H':
+		case 'h':
+			if (scanf("%i", &hi_reg_value) != 1){
+				break;
+			}
+			CURRENT_STATE.HI = hi_reg_value; 
+			NEXT_STATE.HI = hi_reg_value; 
+			break;
+		case 'L':
+		case 'l':
+			if (scanf("%i", &lo_reg_value) != 1){
+				break;
+			}
+			CURRENT_STATE.LO = lo_reg_value;
+			NEXT_STATE.LO = lo_reg_value;
+			break;
+		case 'P':
+		case 'p':
+			print_program(); 
+			break;
+		default:
+			printf("Invalid Command.\n");
+			break;
+	}
+}
+
+/***************************************************************/
+/* reset registers/memory and reload program                                                    */
+/***************************************************************/
+void reset() {   
+	int i;
+	/*reset registers*/
+	for (i = 0; i < MIPS_REGS; i++){
+		CURRENT_STATE.REGS[i] = 0;
+	}
+	CURRENT_STATE.HI = 0;
+	CURRENT_STATE.LO = 0;
+	
+	for (i = 0; i < NUM_MEM_REGION; i++) {
+		uint32_t region_size = MEM_REGIONS[i].end - MEM_REGIONS[i].begin + 1;
+		memset(MEM_REGIONS[i].mem, 0, region_size);
+	}
+	
+	/*load program*/
+	load_program();
+	
+	/*reset PC*/
+	INSTRUCTION_COUNT = 0;
+	CURRENT_STATE.PC =  MEM_TEXT_BEGIN;
+	NEXT_STATE = CURRENT_STATE;
+	RUN_FLAG = TRUE;
+}
+
+/***************************************************************/
+/* Allocate and set memory to zero                                                                            */
+/***************************************************************/
+void init_memory() {                                           
+	int i;
+	for (i = 0; i < NUM_MEM_REGION; i++) {
+		uint32_t region_size = MEM_REGIONS[i].end - MEM_REGIONS[i].begin + 1;
+		MEM_REGIONS[i].mem = malloc(region_size);
+		memset(MEM_REGIONS[i].mem, 0, region_size);
+	}
+}
+
+/**************************************************************/
+/* load program into memory                                                                                      */
+/**************************************************************/
+void load_program() {                   
+	FILE * fp;
+	int i, word;
+	uint32_t address;
+
+	/* Open program file. */
+	fp = fopen(prog_file, "r");
+	if (fp == NULL) {
+		printf("Error: Can't open program file %s\n", prog_file);
+		exit(-1);
+	}
+
+	/* Read in the program. */
+
+	i = 0;
+	while( fscanf(fp, "%x\n", &word) != EOF ) {
+		address = MEM_TEXT_BEGIN + i;
+		mem_write_32(address, word);
+		printf("writing 0x%08x into address 0x%08x (%d)\n", word, address, address);
+		i += 4;
+	}
+	PROGRAM_SIZE = i/4;
+	printf("Program loaded into memory.\n%d words written into memory.\n\n", PROGRAM_SIZE);
+	fclose(fp);
+}
+
+/************************************************************/
+/* decode and execute instruction                                                                     */ 
+/************************************************************/
+void handle_instruction()
+{
+	/*IMPLEMENT THIS*/
+	/* execute one instruction at a time. Use/update CURRENT_STATE and and NEXT_STATE, as necessary.*/
+}
+
+
+/************************************************************/
+/* Initialize Memory                                                                                                    */ 
+/************************************************************/
+void initialize() { 
+	init_memory();
+	CURRENT_STATE.PC = MEM_TEXT_BEGIN;
+	NEXT_STATE = CURRENT_STATE;
+	RUN_FLAG = TRUE;
+}
+
+/************************************************************/
+/* Print the program loaded into memory (in MIPS assembly format)    */ 
+/************************************************************/
+void print_program(){
+	int i;
+	uint32_t addr;
+	
+	for(i=0; i<PROGRAM_SIZE; i++){
+		addr = MEM_TEXT_BEGIN + (i*4);
+		printf("[0x%x]\t", addr);
+		print_instruction(addr);
+	}
+}
+
+/************************************************************/
+/* Print the instruction at given memory address (in MIPS assembly format)    */
+/************************************************************/
+void print_instruction(uint32_t addr){
+	/*IMPLEMENT THIS*/
+
+	char string[64];
+	
+	uint32_t instruction = mem_read32(addr);
+	
+	uint32_t op = instruction & 0xFC000000; //opcode
+	uint32_t bk = instruction & 0x0000003F; //back of 32 bits to id the op code
+	uint32_t rs = instruction & 0x03E00000; //source
+	uint32_t rt = instruction & 0x001F0000; //destination
+	uint16_t im = instruction & 0x0000FFFF; //immediate
+	uint32_t tr = instruction & 0x03FFFFFF; //target
+	uint32_t rd = instruction & 0x00003C00; //reg destination (register)
+	uint32_t sh = instruction & 0x000007C0; //shift amount
+	switch(op) {
+		case 0x00000000: //ADD, ADDU, AND, NOR, OR, SLT, SUB, SUBU, XOR, SLL, SRL, DIV, DIVU, MULT, MULTU, SRA, MFHI, MFLO, MTHI, MTLO, JR, JALR
+			switch(bk) {
+				case 0x00000020; //ADD
+					sprintf(string, "ADD %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000021; //ADDU
+					sprintf(string, "ADDU %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000023; //AND
+					sprintf(string, "AND %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000027; //NOR
+					sprintf(string, "NOR %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000025; //OR
+					sprintf(string, "OR %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x0000002A; //SLT
+					sprintf(string, "SLT %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000022; //SUB
+					sprintf(string, "SUB %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000023; //SUBU
+					sprintf(string, "SUBU %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000026; //XOR
+					sprintf(string, "XOR %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000000; //SLL
+					sprintf(string, "SLL %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000002; //SRL
+					sprintf(string, "SRL %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x0000001A; //DIV
+					sprintf(string, "DIV %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x0000001B; //DIVU
+					sprintf(string, "DIVU %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000018; //MULT
+					sprintf(string, "MULT %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000019; //MULTU
+					sprintf(string, "MULTU %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000003; //SRA
+					sprintf(string, "SRA %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000010; //MFHI
+					sprintf(string, "MFHI %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000012; //MFLO
+					sprintf(string, "MFLO %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000011; //MTHI
+					sprintf(string, "MTHI %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000013; //MTLO
+					sprintf(string, "MTLO %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000008; //JR
+					sprintf(string, "JR %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x00000009; //JALR
+					sprintf(string, "JALR %d, %d, %d\n", rd, rs, rt);
+					break;
+				case 0x0000000C; //SYSTEM CALL
+					sprintf(string, "SYSTEMCALL");
+					break;
+			}
+			break;
+		case 0x		
+	}
+	
+}
+
+/***************************************************************
+				main                                                           
+***************************************************************/
+int main(int argc, char *argv[]) {                              
+	printf("\n**************************\n");
+	printf("Welcome to MU-MIPS SIM...\n");
+	printf("**************************\n\n");
+	
+	if (argc < 2) {
+		printf("Error: You should provide input file.\nUsage: %s <input program> \n\n",  argv[0]);
+		exit(1);
+	}
+
+	strcpy(prog_file, argv[1]);
+	initialize();
+	load_program();
+	help();
+	while (1){
+		handle_command();
+	}
+	return 0;
+}
